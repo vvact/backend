c4856b66cad6ecdf79f3c239faf8e0c5e0ee604e
vvact <vbm7752@gmail.com>
2025-07-18 03:58:37 +0300

    new commit

--- ecommerce_backend/celery.py
+++ ecommerce_backend/celery.py
@@ -2,9 +2,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' command-line program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_backend.settings')
 
-app = Celery('ecommerce_api')
+app = Celery('ecommerce_backend')
 
 # Load task modules from all registered Django app configs.
 app.config_from_object('django.conf:settings', namespace='CELERY')
